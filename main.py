619bdf3bab7b4efe76eb6f77ff79e81bfc5b1720
Sebastian <sebastian.hollabaugh@gmail.com>
2020-08-12 14:05:35 -0400

    Initialized VENV

---
+++ main.py
@@ -0,0 +1,6 @@
+#---------------------------------
+#Project: Python Chess
+#Authors: Sebastian Hollabaugh & Austin Koehler
+#Date: 8-12-2020
+#Desc: A basic game of chess made in python
+#----------------------------------
