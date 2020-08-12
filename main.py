bdefae56827f4e7430fe1fac48c23cdc79774f4b
Sebastian <sebastian.hollabaugh@gmail.com>
2020-08-12 14:52:03 -0400

    Started the game application

--- main.py
+++ main.py
@@ -4,3 +4,32 @@
 #Date: 8-12-2020
 #Desc: A basic game of chess made in python
 #----------------------------------
+
+import pygame
+import sys
+
+pygame.init()
+
+Width = 800
+Height = 600
+
+RED = (255,0,0)
+player_pos = [400, 300]
+player_size = 50
+
+screen = pygame.display.set_mode((Width, Height))
+
+game_over = False
+
+while not game_over:
+    
+    for event in pygame.event.get():
+        if event.type == pygame.quit():
+            sys.exit()
+
+    pygame.draw.rect(screen, RED, (player_pos[0], player_pos[1], player_size, player_size))
+
+    pygame.display.update()
+
+    print(pygame.get_init())
+
